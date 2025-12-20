69a4d19a79db743899eaa552d0e64c0c13871207
bala-amypo <bala@amypo.in>
2025-12-20 04:29:34 +0000

    User commit

--- src/main/java/com/example/demo/service/StudentProfileService.java
+++ src/main/java/com/example/demo/service/StudentProfileService.java
@@ -4,3 +4,11 @@ import com.example.demo.model.StudentProfile;
 import com.example.demo.repository.StudentProfileRepository;
 import org.springframework.stereotype.Service;
 import java.util.List;
+@Service
+public class StudentPeofileService{
+    private final StudentProfileRepository repo;
+    public StudentProfileService(StudentProfileRepository repo){
+        this.repo=repo;
+    }
+    public 
+}
