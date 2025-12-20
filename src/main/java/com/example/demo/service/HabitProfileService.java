26dc0efb7858dcdc1f7d64a5066fbf427913400c
bala-amypo <bala@amypo.in>
2025-12-20 13:54:38 +0000

    User commit

--- src/main/java/com/example/demo/service/HabitProfileService.java
+++ src/main/java/com/example/demo/service/HabitProfileService.java
@@ -1,26 +1,43 @@
-package com.example.demmo.service;
+package com.example.demo.service;
+
 import com.example.demo.exception.ResourceNotFoundException;
 import com.example.demo.model.HabitProfile;
 import com.example.demo.repository.HabitProfileRepository;
 import org.springframework.stereotype.Service;
+
 import java.util.List;
 
 @Service
-public class HabitprofileService{
-    private final HabitProfilerepository repo;
-    public HabitprofileService(HabitProfileRepository repo){
+public class HabitProfileService {
+
+    private final HabitProfileRepository repo;
+
+    public HabitProfileService(HabitProfileRepository repo) {
+        this.repo = repo;
+    }
+
+    public HabitProfile create(HabitProfile h) {
         return repo.save(h);
     }
-    public HabitProfile create(HabitProfile h){
-        return repo.save(h);
+
+    public HabitProfile getById(Long id) {
+        return repo.findById(id)
+                .orElseThrow(() -> new ResourceNotFoundException("Habit not found"));
     }
-    public HabitProfile getById(Long id){
-        return repo findById(id).orElseThrow(()->new ResourceNotFoundException("Habit not Found"));
-    }
-    public List<HabitProfile> getAll(){
+
+    public List<HabitProfile> getAll() {
         return repo.findAll();
     }
-    public HabitProfile update(Long id,HabitProfile updated){
-        HabitProfile h =
+
+    public HabitProfile update(Long id, HabitProfile updated) {
+        HabitProfile h = getById(id);
+        h.setSmoking(updated.isSmoking());
+        h.setDrinking(updated.isDrinking());
+        h.setCleanlinessLevel(updated.getCleanlinessLevel());
+        return repo.save(h);
     }
+
+    public void delete(Long id) {
+        repo.delete(getById(id));
+    }
 }
