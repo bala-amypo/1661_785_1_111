554166b116d04dabf535857e1cdd748f42f78ab3
bala-amypo <bala@amypo.in>
2025-12-20 04:41:34 +0000

    User commit

---
+++ src/main/java/com/example/demo/service/HabitProfileService.java
@@ -0,0 +1,5 @@
+package com.example.demmo.service;
+import com.example.demo.exception.ResourceNotFoundException;
+import com.example.demo.model.HabitProfile;
+import com.example.demo.repository.HabitProfileRepository;
+import org.springframework.stereotyo
