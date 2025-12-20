8bec32542fbc761548eb9524d927630fde706a42
bala-amypo <bala@amypo.in>
2025-12-20 14:51:35 +0000

    User commit

--- src/main/java/com/example/demo/model/StudentProfile.java
+++ src/main/java/com/example/demo/model/StudentProfile.java
@@ -1,19 +1,8 @@
-package com.example.demo.model;
+package com.example.demo.repository;
 
-import jakarta.persistence.*;
-import lombok.Getter;
-import lombok.Setter;
+import com.example.demo.model.StudentProfile;
+import org.springframework.data.jpa.repository.JpaRepository;
 
-@Getter
-@Setter
-@Entity
-public class StudentProfile{
-    @Id
-    @GeneratedValue(strategy=GenerationType.IDENTITY)
-    private Long id;
-    @OneToOne
-    private UserAccount userAccount;
-    private String name;
-    private int age;
-    private String course;
+public interface StudentProfileRepository
+        extends JpaRepository<StudentProfile, Long> {
 }
