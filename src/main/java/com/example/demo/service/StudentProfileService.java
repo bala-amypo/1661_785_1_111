94e3207cd3b95788d9b2cfd8ef7e718c70c6699c
bala-amypo <bala@amypo.in>
2025-12-20 03:22:35 +0000

    User commit

--- src/main/java/com/example/demo/service/StudentProfileService.java
+++ src/main/java/com/example/demo/service/StudentProfileService.java
@@ -2,4 +2,5 @@ package com.example.demo.service;
 import com.example.demo.exception.ResourceNotFoundException;
 import com.example.demo.model.StudentProfile;
 import com.example.demo.repository.StudentProfileRepository;
-import org.springframework.stereotype.Service
+import org.springframework.stereotype.Service;
+import java.util.List;
