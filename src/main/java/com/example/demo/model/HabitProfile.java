60e409d79eeb9b14db7fd59113d0867ba6ffd000
bala-amypo <bala@amypo.in>
2025-12-20 15:03:03 +0000

    User commit

--- src/main/java/com/example/demo/model/HabitProfile.java
+++ src/main/java/com/example/demo/model/HabitProfile.java
@@ -1,20 +1,27 @@
 package com.example.demo.model;
 
 import jakarta.persistence.*;
-import lombok.Getter;
-import lombok.Setter;
 
-@Getter
-@Setter
 @Entity
+public class HabitProfile {
 
-public class HabitProfile{
     @Id
-    @generatedValue(strategy=GenerationType.IDENTITY)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-    @OneToOne
-    private StudentProfile student;
+
     private boolean smoking;
     private boolean drinking;
     private int cleanlinessLevel;
+
+    public boolean isSmoking() {
+        return smoking;
+    }
+
+    public boolean isDrinking() {
+        return drinking;
+    }
+
+    public int getCleanlinessLevel() {
+        return cleanlinessLevel;
+    }
 }
--- src/main/java/com/example/demo/model/UserAccount.java
+++ src/main/java/com/example/demo/model/UserAccount.java
@@ -1,17 +1,14 @@
 package com.example.demo.model;
 
 import jakarta.persistence.*;
-import lombok.Getter;
-import lombok.Setter;
 
-@Getter
-@Setter
 @Entity
-public class UserAccount{
+public class UserAccount {
+
     @Id
-    @GenerataedValue(strategy=GenerationType.IDENTITY)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-    @Column(unique = true)
-    private String email;
+
+    private String username;
     private String password;
 }
