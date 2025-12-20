2b19683f1f678e3a3b6e30081b8641e9cfb30df4
bala-amypo <bala@amypo.in>
2025-12-20 03:01:13 +0000

    User commit

--- src/main/java/com/example/demo/model/StudentProfile.java
+++ src/main/java/com/example/demo/model/StudentProfile.java
@@ -1,1 +1,17 @@
-package com.example.demo
+package com.example.demo.model;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Entity
+public class StudentProfile{
+    @Id
+    @GeneratedValue(strategy=GenerationType.IDENTITY)
+    private Long id;
+    @OneToOne
+    private UserAccount userAccount;
+    private String
+}
