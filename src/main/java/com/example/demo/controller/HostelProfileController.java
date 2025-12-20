6ab35ed51eb9ff575621b3b946fdb502d023be8e
bala-amypo <bala@amypo.in>
2025-12-20 13:57:29 +0000

    User commit

---
+++ src/main/java/com/example/demo/controller/HostelProfileController.java
@@ -0,0 +1,44 @@
+package com.example.demo.controller;
+
+import com.example.demo.model.HabitProfile;
+import com.example.demo.service.HabitProfileService;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/habits")
+public class HabitProfileController {
+
+    private final HabitProfileService service;
+
+    public HabitProfileController(HabitProfileService service) {
+        this.service = service;
+    }
+
+    @PostMapping
+    public HabitProfile create(@RequestBody HabitProfile h) {
+        return service.create(h);
+    }
+
+    @GetMapping("/{id}")
+    public HabitProfile get(@PathVariable Long id) {
+        return service.getById(id);
+    }
+
+    @GetMapping
+    public List<HabitProfile> getAll() {
+        return service.getAll();
+    }
+
+    @PutMapping("/{id}")
+    public HabitProfile update(@PathVariable Long id,
+                               @RequestBody HabitProfile h) {
+        return service.update(id, h);
+    }
+
+    @DeleteMapping("/{id}")
+    public void delete(@PathVariable Long id) {
+        service.delete(id);
+    }
+}
---
+++ src/main/java/com/example/demo/controller/StudentProfileController.java
@@ -0,0 +1,44 @@
+package com.example.demo.controller;
+
+import com.example.demo.model.StudentProfile;
+import com.example.demo.service.StudentProfileService;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/students")
+public class StudentProfileController {
+
+    private final StudentProfileService service;
+
+    public StudentProfileController(StudentProfileService service) {
+        this.service = service;
+    }
+
+    @PostMapping
+    public StudentProfile create(@RequestBody StudentProfile p) {
+        return service.create(p);
+    }
+
+    @GetMapping("/{id}")
+    public StudentProfile get(@PathVariable Long id) {
+        return service.getById(id);
+    }
+
+    @GetMapping
+    public List<StudentProfile> getAll() {
+        return service.getAll();
+    }
+
+    @PutMapping("/{id}")
+    public StudentProfile update(@PathVariable Long id,
+                                 @RequestBody StudentProfile p) {
+        return service.update(id, p);
+    }
+
+    @DeleteMapping("/{id}")
+    public void delete(@PathVariable Long id) {
+        service.delete(id);
+    }
+}
