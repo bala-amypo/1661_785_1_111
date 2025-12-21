77275ec269e9dad84c85e09510962db39ddb2371
bala-amypo <bala@amypo.in>
2025-12-21 02:41:41 +0000

    User commit

--- src/main/java/com/example/demo/model/StudentProfile.java
+++ src/main/java/com/example/demo/model/StudentProfile.java
@@ -9,23 +9,14 @@ public class StudentProfile {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @OneToOne
+    private UserAccount userAccount;
+
     private String name;
     private int age;
     private String course;
     private String gender;
     private String roomTypePreference;
 
-    public Long getId() { return id; }
-    public String getName() { return name; }
-    public int getAge() { return age; }
-    public String getCourse() { return course; }
-    public String getGender() { return gender; }
-    public String getRoomTypePreference() { return roomTypePreference; }
-
-    public void setId(Long id) { this.id = id; }
-    public void setName(String name) { this.name = name; }
-    public void setAge(int age) { this.age = age; }
-    public void setCourse(String course) { this.course = course; }
-    public void setGender(String gender) { this.gender = gender; }
-    public void setRoomTypePreference(String roomTypePreference) { this.roomTypePreference = roomTypePreference; }
+    // getters & setters
 }
