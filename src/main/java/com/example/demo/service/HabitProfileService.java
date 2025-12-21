8d04b7d6974204920cac3e84a771725448cba81f
bala-amypo <bala@amypo.in>
2025-12-21 03:14:41 +0000

    User commit

--- src/main/java/com/example/demo/service/HabitProfileService.java
+++ src/main/java/com/example/demo/service/HabitProfileService.java
@@ -1,8 +1,17 @@
 package com.example.demo.service;
 
+import java.util.List;
 import com.example.demo.model.HabitProfile;
 
 public interface HabitProfileService {
-    HabitProfile createOrUpdate(Long studentId, HabitProfile habit);
-    HabitProfile getForStudent(Long studentId);
+
+    HabitProfile create(HabitProfile habit);
+
+    HabitProfile update(Long id, HabitProfile habit);
+
+    HabitProfile getById(Long id);
+
+    List<HabitProfile> getAll();
+
+    void delete(Long id);
 }
