3682c1aa0612476e52207926aa4c4ed2dc9ffc87
bala-amypo <bala@amypo.in>
2025-12-21 02:58:39 +0000

    User commit

--- src/main/java/com/example/demo/model/HabitProfile.java
+++ src/main/java/com/example/demo/model/HabitProfile.java
@@ -1,6 +1,10 @@
 package com.example.demo.model;
 
-import jakarta.persistence.*;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToOne;
 
 @Entity
 public class HabitProfile {
@@ -17,5 +21,54 @@ public class HabitProfile {
     private int cleanlinessLevel;
     private int noisePreference;
 
-    // getters & setters
+    public HabitProfile() {
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public StudentProfile getStudent() {
+        return student;
+    }
+
+    public void setStudent(StudentProfile student) {
+        this.student = student;
+    }
+
+    public boolean isSmoking() {
+        return smoking;
+    }
+
+    public void setSmoking(boolean smoking) {
+        this.smoking = smoking;
+    }
+
+    public boolean isDrinking() {
+        return drinking;
+    }
+
+    public void setDrinking(boolean drinking) {
+        this.drinking = drinking;
+    }
+
+    public int getCleanlinessLevel() {
+        return cleanlinessLevel;
+    }
+
+    public void setCleanlinessLevel(int cleanlinessLevel) {
+        this.cleanlinessLevel = cleanlinessLevel;
+    }
+
+    public int getNoisePreference() {
+        return noisePreference;
+    }
+
+    public void setNoisePreference(int noisePreference) {
+        this.noisePreference = noisePreference;
+    }
 }
