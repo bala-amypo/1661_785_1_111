a30b1dfe49a775875bafaf43d9b13f9195d680bd
bala-amypo <bala@amypo.in>
2025-12-21 02:41:53 +0000

    User commit

--- src/main/java/com/example/demo/model/HabitProfile.java
+++ src/main/java/com/example/demo/model/HabitProfile.java
@@ -9,20 +9,13 @@ public class HabitProfile {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @OneToOne
+    private StudentProfile student;
+
     private boolean smoking;
     private boolean drinking;
     private int cleanlinessLevel;
     private int noisePreference;
 
-    public Long getId() { return id; }
-    public boolean isSmoking() { return smoking; }
-    public boolean isDrinking() { return drinking; }
-    public int getCleanlinessLevel() { return cleanlinessLevel; }
-    public int getNoisePreference() { return noisePreference; }
-
-    public void setId(Long id) { this.id = id; }
-    public void setSmoking(boolean smoking) { this.smoking = smoking; }
-    public void setDrinking(boolean drinking) { this.drinking = drinking; }
-    public void setCleanlinessLevel(int cleanlinessLevel) { this.cleanlinessLevel = cleanlinessLevel; }
-    public void setNoisePreference(int noisePreference) { this.noisePreference = noisePreference; }
+    // getters & setters
 }
