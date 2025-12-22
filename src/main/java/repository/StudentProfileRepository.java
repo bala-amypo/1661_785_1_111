696273fdabdda289b72fc44d9895f02605e4da52
bala-amypo <bala@amypo.in>
2025-12-22 07:47:02 +0000

    User commit

--- src/main/java/repository/StudentProfileRepository.java
+++ src/main/java/repository/StudentProfileRepository.java
@@ -1,9 +1,7 @@
 package com.example.demo.repository;
 
 import com.example.demo.model.StudentProfile;
-import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface StudentProfileRepository extends JpaRepository<StudentProfile, Long> {
-    Optional<StudentProfile> findByUserAccountId(Long userId);
 }
