d161eb0cd7296dea1b8f9cffacd2c2da63c0ffa1
bala-amypo <bala@amypo.in>
2025-12-26 04:17:57 +0000

    User commit

--- src/main/java/repository/MatchResultRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.demo.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import com.example.demo.model.MatchResult;
-import java.util.List;
-
-public interface MatchResultRepository extends JpaRepository<MatchResult, Long> {
-    List<MatchResult> findByStudentAIdOrStudentBIdOrderByScoreDesc(Long studentAId, Long studentBId);
-}
--- src/main/java/repository/UserAccountRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.demo.repository;
-
-import com.example.demo.model.UserAccount;
-import java.util.Optional;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserAccountRepository extends JpaRepository<UserAccount, Long> {
-    Optional<UserAccount> findByEmail(String email);
-}
