80593426524c26c85122cd9b74bda1f24f119254
bala-amypo <bala@amypo.in>
2025-12-26 04:59:56 +0000

    User commit

--- src/main/java/com/example/demo/service/StudentProfileService.java
+++ src/main/java/com/example/demo/service/StudentProfileService.java
@@ -1,17 +1,19 @@
 package com.example.demo.service;
 
 import com.example.demo.model.StudentProfile;
-import java.util.*;
+
+import java.util.List;
+import java.util.Optional;
 
 public interface StudentProfileService {
 
-    StudentProfile createStudent(StudentProfile student);
+    StudentProfile createStudent(StudentProfile s);
+
+    StudentProfile updateStudentStatus(Long id, boolean active);
 
     StudentProfile getStudentById(Long id);
 
     List<StudentProfile> getAllStudents();
-
-    StudentProfile updateStudentStatus(Long id, boolean active);
 
     Optional<StudentProfile> findByStudentId(String studentId);
 }
